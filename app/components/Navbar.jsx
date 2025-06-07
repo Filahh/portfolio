e56b23301306b1f0f08c6afdb663eb15dc802183
henry victor <henryvictor215@gmail.com>
2025-06-07 10:08:05 +0100

    project completed

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
             <nav 
             className={`w-full fixed px-5 lg:px-8 xl:px-{8%} py-4 flex items-center justify-between z-50 ${isScroll ? "bg-white bg-opacity-50 backdrop-blur-lg shadow-sm " : ""}`}>
                 <a href="#top">
-                    <Image src={assets.logo} alt='' className='w-28 cursor-pointer mr-10' />
+                    <Image src={assets.henry_logo} alt='' className='w-28 cursor-pointer mr-10' />
                 </a>
 
                 <ul className={`hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 ${isScroll ? "" : "bg-white shadow-sm bg-opacity-50"}`}>
--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -1,19 +1,41 @@
 import { assets, serviceData } from '@/assets/assets'
 import Image from 'next/image'
 import React from 'react'
+import { motion } from 'motion/react'
 
 const Services = () => {
   return (
-    <div id='services' className='w-full px-[12%] py-10 scroll-mt-20 mt-32'>
-        <h4 className='text-center mb-2 text-lg font-Ovo'>What I Offer</h4>
-        <h2 className='text-center text-5xl font-Ovo'>My Services</h2>
+    <motion.div 
+    initial={{ opacity: 0}}
+    whileInView={{ opacity: 1 }}
+    transition={{ duration: 1}}
+    id='services' className='w-full px-[12%] py-10 scroll-mt-20'>
+        <motion.h4 
+        initial={{ y: -20,opacity: 0}}
+        whileInView={{ y:0, opacity: 1 }}
+        transition={{ delay: 0.3, duration: 0.5}}
+        className='text-center mb-2 text-lg font-Ovo'>What I Offer</motion.h4>
+        <motion.h2 
+        initial={{ y: -20,opacity: 0}}
+        whileInView={{ y:0, opacity: 1 }}
+        transition={{ delay: 0.5, duration: 0.5}}
+        className='text-center text-5xl font-Ovo'>My Services</motion.h2>
 
-        <p className="text-center max-2xl mx-auto mt-5 mb-12 font-Ovo">
+        <motion.p 
+        initial={{ opacity: 0}}
+        whileInView={{ opacity: 1 }}
+        transition={{ delay: 0.7, duration: 0.5}}
+        className="text-center max-2xl mx-auto mt-5 mb-12 font-Ovo">
             I am a Frontend Developer with over a decade of experience in the field. I have had the privilege of collaborating with prestigious organizations, contributing to their success and growth. My expertise lies in creating visually stunning and user-friendly websites that leave a lasting impression on visitors. 
-        </p>
-        <div className="grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-6 my-10">
+        </motion.p>
+        <motion.div 
+        initial={{ opacity: 0}}
+        whileInView={{ opacity: 1 }}
+        transition={{ delay: 0.9, duration: 0.6}}
+        className="grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-6 my-10">
             {serviceData.map(({icon, title, description, link},index)=>(
-                <div 
+                <motion.div 
+                whileHover={{ scale: 1.05 }}
                 key={index} 
                 className="border border-gray-300 rounded-lg px-8 py-12 cursor-pointer transition-transform duration-500 
                 shadow-md hover:shadow-xl hover:shadow-black/30 hover:-translate-y-2 bg-white hover:bg-lightHover"
@@ -21,13 +43,12 @@ const Services = () => {
                     <Image src={icon} alt='icon' className='w-10' />
                     <h3 className="text-lg my-4 text-gray-700">{title}</h3>
                     <p className="text-sm text-gray-600 leading-5">{description}</p>
-                    <a href={link} className='flex items-center gap-2 text-sm mt-5'>Read More <Image src={assets.right_arrow} alt='icon' className='w-4' /></a>
-                </div>
+                </motion.div>
             ))}
 
-        </div>
+        </motion.div>
         
-    </div>
+    </motion.div>
   )
 }
 
